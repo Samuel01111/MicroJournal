cd33bc5e525cb65909e2109970f7faf7805d9f42
Samuel <Samuel.nascimento.2015@hotmail.com>
2025-11-28 21:26:13 -0300

    Implementing the voice recorder

--- app/src/main/java/com/plcoding/echojournal/app/MainActivity.kt
+++ app/src/main/java/com/plcoding/echojournal/app/MainActivity.kt
@@ -1,21 +1,20 @@
-package com.plcoding.echojournal
+package com.plcoding.echojournal.app
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.core.app.ActivityCompat
 import com.plcoding.echojournal.core.presentation.designsystem.theme.EchoJournalTheme
-import com.plcoding.echojournal.core.presentation.designsystem.theme.bgGradient
+import com.plcoding.echojournal.echos.data.recording.AndroidVoiceRecorder
 import com.plcoding.echojournal.echos.presentation.echos.EchosRoot
 
 class MainActivity : ComponentActivity() {
@@ -24,7 +23,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             EchoJournalTheme {
-                EchosRoot()
+
             }
         }
     }
